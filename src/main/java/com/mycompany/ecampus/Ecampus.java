3b8cad25c96bc5fe51c6b4c5bafecf505b07d8c8
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-10-21 16:07:52 +0530

    Assign variables values & print

--- src/main/java/com/mycompany/ecampus/Ecampus.java
+++ src/main/java/com/mycompany/ecampus/Ecampus.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package com.mycompany.ecampus;
 
 /**
@@ -10,8 +9,29 @@ package com.mycompany.ecampus;
  * @author ASUS
  */
 class Ecampus {
-public static void main (String [] args) {
-System.out.println("Hello E campus");
-}
-   
+
+    public static void main(String[] args) {
+
+        int myNumber = 10;
+
+        int myNum;
+        myNum = 15;
+
+        char character = 'w';
+
+        boolean bool = true;
+
+        float myfloat = 4.5f;
+
+        double myDouble = 4.52342;
+
+        System.out.println(myNumber);
+        System.out.println(myNum);
+        System.out.println(character);
+        System.out.println(bool);
+        System.out.println(myfloat);
+        System.out.println(myDouble);
+
+    }
+
 }
