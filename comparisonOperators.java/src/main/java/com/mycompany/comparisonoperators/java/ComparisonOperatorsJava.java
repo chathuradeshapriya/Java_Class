2ca10ff0a0340b8c87605059fe70135adb6f6b2a
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-10-31 23:44:12 +0530

    Add files via upload
    Comparison Operators

---
+++ comparisonOperators.java/src/main/java/com/mycompany/comparisonoperators/java/ComparisonOperatorsJava.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.comparisonoperators.java;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ComparisonOperatorsJava {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 5;
+        int z = 6;
+        
+ 
+        System.out.println(x == y);
+        System.out.println(x == z);
+        System.out.println(x != y);
+        System.out.println(x <= y);
+        System.out.println(x >= z);
+        System.out.println(x > y);
+    }
+}
