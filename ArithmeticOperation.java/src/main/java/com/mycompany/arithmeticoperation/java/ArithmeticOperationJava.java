f81a87bb94b448a07baf37b9aabe2ee2233cc3bb
Chathura Deshapriya <57343967+chathuradeshapriya@users.noreply.github.com>
2022-10-31 23:42:11 +0530

    Add files via upload
    Arithmetic Operation

---
+++ ArithmeticOperation.java/src/main/java/com/mycompany/arithmeticoperation/java/ArithmeticOperationJava.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.arithmeticoperation.java;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ArithmeticOperationJava {
+
+    public static void main (String [] args) {
+    
+    int num1 = 10;
+    int num2 = 20;
+    
+    int resultsPlus = num1 + num2;
+    int resultsminus = num2 - num1;
+    int resultsmulti = num1 * num2;
+    int resultdevide = num2 / num1;
+    
+    System.out.println("Answer : " + resultsPlus);
+    System.out.println("Answer : " + resultsminus);
+    System.out.println("Answer : " + resultsmulti);
+    System.out.println("Answer : " +  resultdevide );
+            
+    
+    
+    }
+    
+    
+}
